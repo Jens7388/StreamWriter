c55c9ff919d9ceddea08cc37963f06065d9b26f5
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-01-13 09:36:29 +0100

    Add project files.

---
+++ Opgave1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Opgave1
+{
+    class Program
+    {
+        private static bool WriteToFile(string path)
+        {
+            bool fileExists = File.Exists(path);
+            if(fileExists)
+            {
+                using(StreamWriter sr = File.AppendText(path))
+                {
+                    Console.Write("Skriv noget tekst: ");
+                    DateTime today = DateTime.Now;
+                    string input = Console.ReadLine();
+                    sr.WriteLine($"{today.ToLocalTime()}: {input}");
+                    sr.Close();
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Ugyldig sti! prøv igen");
+                return false;
+            }
+        }
+        private static void Main()
+        {
+            while(true)
+            {
+                Console.Write("Indtast filens sti: ");
+                string fileInput = Console.ReadLine();
+                WriteToFile(fileInput);
+                Console.WriteLine("Modtaget. Dit dokument er blevet opdateret");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            
+        }
+    }
+}
---
+++ Opgave2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Opgave2
+{
+    class Program
+    {
+        private static bool WriteToFile(string path)
+        {
+            bool fileExists = File.Exists(path);
+            if(fileExists)
+            {
+                using(StreamWriter sr = File.AppendText(path))
+                {
+                    Console.Write("Skriv mindste værdi til talgeneratoren: ");
+                    string input = Console.ReadLine();
+                    int.TryParse(input, out int lowestNumber);
+
+                    Console.Write("Skriv største værdi til talgeneratoren: ");
+                    input = Console.ReadLine();
+                    int.TryParse(input, out int highestNumber);
+
+                    Console.Write("Skriv antallet af værdier til talgeneratorer: ");
+                    input = Console.ReadLine();
+                    int.TryParse(input, out int numbers);
+                    Random generator = new Random();
+
+                    for(int i = 0; i < numbers; i++)
+                    {
+                        sr.WriteLine(generator.Next(lowestNumber, highestNumber));                       
+                    }
+                    sr.Close();
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Ugyldig sti! prøv igen");
+                return false;
+            }
+        }
+        private static void Main()
+        {
+            while(true)
+            {
+                Console.Write("Indtast filens sti: ");
+                string fileInput = Console.ReadLine();
+                WriteToFile(fileInput);
+                Console.WriteLine("Dine tal er genereret!");
+                Console.ReadLine();
+                Console.Clear();
+            }          
+        }
+    }
+}
